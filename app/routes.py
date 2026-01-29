a7ff21891d30d732d96491158d701d7857e162f9
elaine <yeelaine@uchicago.edu>
2026-01-28 20:32:02 -0600

    test flake8 formatting

--- app/routes.py
+++ app/routes.py
@@ -91,7 +91,7 @@ def register_routes(app):
                 jsonify(
                     {
                         "success": True,
-                        "message": f"{book['title']} checked out successfully",
+                        "message": f"{book['title']} checked out successfully in the inventory",
                         "book": {
                             "isbn": isbn,
                             "title": book["title"],
